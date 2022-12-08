48085f6315ef8d53bd5206c5495c9da4eb251455
Patrik Norlin <patrik.norlin95@gmail.com>
2022-12-09 00:29:01 +0100

    improve day8

--- day08/08.py
+++ day08/08.py
@@ -6,23 +6,32 @@ def part1(grid):
             if (x == 0 or y == 0 or x == SIZE-1 or y == SIZE-1):
                 total += 1
             else:
-                row = [grid[x][y_2] for y_2 in range(SIZE)]
-                col = [grid[x_2][y] for x_2 in range(SIZE)]
-                visible_from_left = len([tree for tree in row[:y] if tree < grid[x][y]]) == len(row[:y])
-                visible_from_right = len([tree for tree in row[y+1:] if tree < grid[x][y]]) == len(row[y+1:])
-                visible_from_top = len([tree for tree in col[:x] if tree < grid[x][y]]) == len(col[:x])
-                visible_from_bottom = len([tree for tree in col[x+1:] if tree < grid[x][y]]) == len(col[x+1:])
+                row = get_row(grid, x)
+                col = get_col(grid, y)
+                visible_from_left = visible(row[:y], grid[x][y])
+                visible_from_right = visible(row[y+1:], grid[x][y])
+                visible_from_top = visible(col[:x], grid[x][y])
+                visible_from_bottom = visible(col[x+1:], grid[x][y])
                 if (visible_from_left or visible_from_right or visible_from_top or visible_from_bottom):
                     total += 1
     return total
+
+def get_row(grid, x):
+    return [grid[x][y_2] for y_2 in range(len(grid))]
+
+def get_col(grid, y):
+    [grid[x_2][y] for x_2 in range(len(grid))]
+
+def visible(trees, tree):
+    return sum([1 for t in trees if t >= tree]) == 0
 
 def part2(grid):
     scores = []
     SIZE = len(grid)
     for x in range(SIZE):
         for y in range(SIZE):
-            row = [grid[x][y_2] for y_2 in range(SIZE)]
-            col = [grid[x_2][y] for x_2 in range(SIZE)]
+            row = get_row(grid, x)
+            col = get_col(grid, y)
             right_score = score(row[y+1:], grid[x][y])
             down_score = score(col[x+1:], grid[x][y])
             up_score = score(list(reversed(col[:x])), grid[x][y])
